bc02af0f4c2c39d6beca48c8204b45f631f2127a
Tarei King <tareiking@gmail.com>
2014-06-28 13:57:50 +1000

    Update template file

--- templates/testimonials-template.php
+++ templates/testimonials-template.php
@@ -17,6 +17,8 @@ $default_css = array(
 // Allow developers to adjust the main query args
 $args	= apply_filters( 'sz_easy_testimonials_defaults', $args );
 
+
+
 // Allow developers to drop in their own classnames
 $css	= apply_filters( 'sz_easy_testimonals_classnames', $_css );
 
@@ -24,20 +26,30 @@ $args	= wp_parse_args( $args, $defaults );
 $css	= wp_parse_args( $css, $default_css );
 
 $testimonials = new WP_Query( $args );
-
+/**
+ * The template starts here...
+ */
 if ( $testimonials->have_posts() ): ?>
 
-	<div class="testimonials">
+	<div class="<?php echo $css['parent_class']; ?>">
 		<ul>
 			<?php while ( $testimonials->have_posts() ) : $testimonials->the_post(); ?>
 
-				<li>
+				<li class="<?php echo $css['item_class']; ?>">
+
+					<?php if ( $defaults['show_title'] ) { ?>
+						<h1 class="<?php echo $css['title_class']; ?>"><?php the_title(); ?></h1>
+					<?php } ?>
 
 					<?php if ( has_post_thumbnail() ): ?>
-						<?php the_post_thumbnail( 'testimonial-thumb' ); ?>
+
+						<?php the_post_thumbnail( 'testimonial-thumb' , array( 'class' => $css['image_class'] ) ); ?>
+
 					<?php endif; ?>
 
-					<?php the_content(); ?>
+					<div class="<?php echo $css['content_class']; ?>">
+						<?php the_content(); ?>
+					</div>
 
 				</li>
 			<?php endwhile; ?>
