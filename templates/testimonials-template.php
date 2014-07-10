6f993534f2384f82c77cc6cbfbc51b979c09d1f4
Tarei King <tareiking@gmail.com>
2014-07-10 14:07:36 +1000

    Update the testimonial template

--- templates/testimonials-template.php
+++ templates/testimonials-template.php
@@ -1,58 +1,49 @@
 <?php
 
-$defaults  = array(
-	'post_type'			=> 'testimonial',
-	'posts_per_page'	=> 5,
-	'show_title'		=> true,
-);
+$plugin_defaults = SZ_Easy_Testimonials_Widget::get_widget_defaults();
 
-$default_css = array(
-	'parent_class'		=> 'testimonials',
-	'item_class'		=> 'testimonial-item',
-	'image_class'		=> 'testimonial-image',
-	'content_class'		=> 'testimonial-content',
-	'title_class'		=> 'testimonial-title',
-);
+$default_css = $plugin_defaults['css'];
+$defaults = $plugin_defaults['args'];
 
 // Allow developers to adjust the main query args
-$args	= apply_filters( 'sz_easy_testimonials_defaults', $args );
+$args	= apply_filters( 'sz_easy_testimonials_defaults', $defaults );
 
 // Allow developers to drop in their own classnames
-$css	= apply_filters( 'sz_easy_testimonals_classnames', $_css );
+$css	= apply_filters( 'sz_easy_testimonals_classnames', $default_css );
 
+// Parse args from both arrays
 $args	= wp_parse_args( $args, $defaults );
 $css	= wp_parse_args( $css, $default_css );
 
+// Run that query...
 $testimonials = new WP_Query( $args );
+
 /**
  * The template starts here...
  */
 if ( $testimonials->have_posts() ): ?>
 
 	<div class="<?php echo $css['parent_class']; ?>">
-		<ul>
+		<ul class="slides">
 			<?php while ( $testimonials->have_posts() ) : $testimonials->the_post(); ?>
 
 				<li class="<?php echo $css['item_class']; ?>">
+					<blockquote>
+						<div class="<?php echo $css['content_class']; ?>">
+							<?php the_content(); ?>
+						</div>
 
-					<?php if ( $defaults['show_title'] ) { ?>
-						<h1 class="<?php echo $css['title_class']; ?>"><?php the_title(); ?></h1>
-					<?php } ?>
+						<?php if ( $defaults['show_title'] ) { ?>
+						<cite class="<?php echo $css['title_class']; ?>"><h3><?php the_title(); ?></h3></cite>
+						<?php } ?>
 
-					<?php if ( has_post_thumbnail() ): ?>
-
-						<?php the_post_thumbnail( 'testimonial-thumb' , array( 'class' => $css['image_class'] ) ); ?>
-
-					<?php endif; ?>
-
-					<div class="<?php echo $css['content_class']; ?>">
-						<?php the_content(); ?>
-					</div>
+					</blockquote>
 
 				</li>
 			<?php endwhile; ?>
 		</ul>
 	</div>
+
 <?php else: ?>
 	<p>No testimonials to display.</p>
 <?php endif; ?>
