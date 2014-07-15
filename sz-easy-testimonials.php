e17907018ae417fda0032a341ab1ea4ccf9777a1
Tarei King <tareiking@gmail.com>
2014-07-15 11:30:15 +1000

    Update display funcitons to support custom query passing

--- sz-easy-testimonials.php
+++ sz-easy-testimonials.php
@@ -87,16 +87,48 @@ private static $instance;
 		return $trimmed;
 	}
 
-	public function do_testimonials() {
+	public static function do_testimonials( $query = false ) {
 		$plugindir        = dirname( __FILE__ );
 		$templatefilename = 'testimonials-template.php';
 		if ( file_exists( TEMPLATEPATH . '/' . $templatefilename ) ) {
 			$return_template = TEMPLATEPATH . '/' . $templatefilename;
-			require_once( $return_template );
+			require( $return_template );
 		} else {
 			$return_template = $plugindir . '/templates/' . $templatefilename;
-			require_once( $return_template );
+			require( $return_template );
 		}
+	}
+
+	public static function get_default_query( $query = '' ){
+
+		if ( $query != '' ) { return $query; }
+
+		$defaults  = array(
+			'post_type'         => 'testimonial',
+			'orderby'           => 'rand',
+			'posts_per_page'    => -1,
+			'show_title'        => true,
+		);
+
+		$default_css = array(
+			'parent_class'      => 'testimonials',
+			'item_class'        => 'testimonial-item',
+			'image_class'       => 'testimonial-image',
+			'content_class'     => 'testimonial-content',
+			'title_class'       => 'testimonial-title',
+		);
+
+		// Allow developers to adjust the main query args
+		$args   = apply_filters( 'sz_easy_testimonials_defaults', $args );
+		// Allow developers to drop in their own classnames
+		$css    = apply_filters( 'sz_easy_testimonals_classnames', $_css );
+
+		$args   = wp_parse_args( $args, $defaults );
+		$css    = wp_parse_args( $css, $default_css );
+
+		$query = new WP_Query( $args );
+
+		return $query;
 	}
 
 }
