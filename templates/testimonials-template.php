566bc8db2e7146333881f34939429b6c4a97cc07
Tarei King <tareiking@gmail.com>
2014-07-15 11:30:49 +1000

    Update default template

--- templates/testimonials-template.php
+++ templates/testimonials-template.php
@@ -1,55 +1,47 @@
 <?php
 
-$defaults  = array(
-	'post_type'			=> 'testimonial',
-	'posts_per_page'	=> 5,
-	'show_title'		=> true,
-);
+/**
+ * If no custom query specified, lets load the default
+ */
 
-$default_css = array(
-	'parent_class'		=> 'testimonials',
-	'item_class'		=> 'testimonial-item',
-	'image_class'		=> 'testimonial-image',
-	'content_class'		=> 'testimonial-content',
-	'title_class'		=> 'testimonial-title',
-);
+	if ( $query == false ){
+		$testimonials 	= SZ_Easy_Testimonials::get_default_query();
+	} else {
+		$testimonials = $query;
+	}
 
-// Allow developers to adjust the main query args
-$args	= apply_filters( 'sz_easy_testimonials_defaults', $args );
+	$css = apply_filters( 'sz_easy_testimonals_classnames', $_css );
 
-// Allow developers to drop in their own classnames
-$css	= apply_filters( 'sz_easy_testimonals_classnames', $_css );
-
-$args	= wp_parse_args( $args, $defaults );
-$css	= wp_parse_args( $css, $default_css );
-
-$testimonials = new WP_Query( $args );
 /**
  * The template starts here...
  */
 if ( $testimonials->have_posts() ): ?>
 
-	<div class="<?php echo $css['parent_class']; ?>">
+	<div class="<?php echo $css['parent_class']; ?> testimonials">
 		<ul>
 			<?php while ( $testimonials->have_posts() ) : $testimonials->the_post(); ?>
 
-				<li class="<?php echo $css['item_class']; ?>">
+			<?php
+				$thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' );
+				$thumb_url = $thumb['0'];
+			?>
+			<li class="<?php echo $css['item_class']; ?>">
+				<?php if ( has_post_thumbnail() ) { ?>
+					<div class="testimonial-thumb-wrapper circular-image" style="background: url(<?php echo $thumb_url; ?>) no-repeat;"></div>
+				<?php } else { ?>
+					<div class="testimonial-thumb-wrapper circular-image" style="background-color: #99cc33;"></div>
+				<?php } ?>
 
-					<?php if ( $defaults['show_title'] ) { ?>
-						<h1 class="<?php echo $css['title_class']; ?>"><?php the_title(); ?></h1>
-					<?php } ?>
+				<div class="<?php echo $css['content_class']; ?>">
 
-					<?php if ( has_post_thumbnail() ): ?>
+					<blockquote>
+						<span class="<?php echo $css['title_class']; ?>"><strong><?php the_title(); ?></strong></span>
+						<?php echo SZ_Easy_Testimonials::get_custom_excerpt( get_the_excerpt() ); ?>
+					</blockquote>
 
-						<?php the_post_thumbnail( 'testimonial-thumb' , array( 'class' => $css['image_class'] ) ); ?>
+				</div>
 
-					<?php endif; ?>
-
-					<div class="<?php echo $css['content_class']; ?>">
-						<?php the_content(); ?>
-					</div>
-
-				</li>
+			</li>
 			<?php endwhile; ?>
 		</ul>
 	</div>
